838cde867716e1e75b6addb36e2929bdc3259906
natalypereiramiranda <110977847+natalypereiramiranda@users.noreply.github.com>
2024-06-06 11:04:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-pergunta");
+const caixaAlternativas = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".texto-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
