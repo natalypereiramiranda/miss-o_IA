20aa75969a487b29b4f16ba2e7e8fb4e92a59826
natalypereiramiranda <110977847+natalypereiramiranda@users.noreply.github.com>
2024-09-05 11:02:38 -0300

    Add files via upload

---
+++ js/aleatorio.js
@@ -0,0 +1,8 @@
+const nomes = ["Marta", "Milena", "Maria Eduarda", "Marcelo", "Evelyn", "Cristiano", "Claudio"];
+
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
---
+++ js/perguntas.js
@@ -0,0 +1,170 @@
+export const perguntas = [
+    {
+        enunciado: " A ergonomia é fundamental para promover a saúde e o bem-estar dos trabalhadores ao adaptar os ambientes e equipamentos de trabalho às necessidades físicas e cognitivas dos indivíduos. Ao implementar práticas ergonômicas eficazes, as organizações podem reduzir o risco de lesões e desconfortos, aumentar a eficiência e a produtividade, e melhorar a satisfação geral dos funcionários. A importância da ergonomia reside em sua capacidade de criar um ambiente de trabalho que não apenas previne problemas de saúde, mas também promove um desempenho otimizado e uma experiência positiva para todos os envolvidos.",
+        alternativas: [
+            {
+                texto: "Quais são as principais consequências da aplicação inadequada dos princípios de ergonomia em um ambiente de trabalho, como um escritório ou uma fábrica?",
+                afirmacao: [
+                    "Aumento das Lesões e Doenças Ocupacionais: A aplicação inadequada dos princípios de ergonomia pode levar a um aumento significativo de lesões e doenças ocupacionais. Em um escritório, por exemplo, cadeiras e mesas mal ajustadas podem causar dores nas costas, no pescoço e nas mãos, resultando em condições como a síndrome do túnel do carpo. Em fábricas, a postura inadequada e a repetição de movimentos podem causar distúrbios musculoesqueléticos e outros problemas de saúde, como tendinites e lombalgias.",
+                  "Redução da Produtividade e Satisfação dos Funcionários: A falta de ergonomia adequada pode impactar negativamente a produtividade e a satisfação dos funcionários. Funcionários que enfrentam desconforto físico devido a um ambiente mal projetado podem ter dificuldade em se concentrar e realizar tarefas de forma eficiente. Isso pode levar a um aumento no absenteísmo, menor qualidade no trabalho e, em última análise, maior rotatividade de funcionários, afetando a eficiência geral da organização."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Como a configuração atual dos postos de trabalho influencia a saúde e o bem-estar dos funcionários, e que ajustes podem ser feitos para melhorar a postura e reduzir a fadiga?",
+                afirmacao: [
+                    " Redução de problemas de saúde relacionados ao trabalho: Ajustes na configuração dos postos de trabalho, como cadeiras ergonômicas e mesas ajustáveis, podem diminuir o risco de dores nas costas, problemas de postura e lesões por esforços repetitivos, melhorando a saúde geral dos funcionários.",                   
+                    "Aumento da produtividade e satisfação no trabalho: Melhorar a ergonomia pode levar a menos desconforto físico e fadiga, resultando em funcionários mais engajados e produtivos. Além disso, isso pode aumentar a satisfação no trabalho e diminuir a rotatividade de pessoal."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: " A ergonomia é a ciência que estuda a adaptação dos ambientes e ferramentas de trabalho às necessidades e capacidades dos indivíduos, com o objetivo de melhorar a eficiência, conforto e segurança no desempenho das atividades. Implementar princípios ergonômicos no local de trabalho pode não apenas prevenir lesões e reduzir desconfortos físicos, mas também aumentar a produtividade e a satisfação dos funcionários ao criar um ambiente mais saudável e funcional.",
+        alternativas: [
+            {
+                texto: " Quais são os principais fatores ergonômicos que devem ser considerados ao projetar um espaço de trabalho para garantir que ele seja eficiente e confortável para os funcionários, e como a integração de tecnologias ergonômicas pode contribuir para a produtividade?",
+                afirmacao: [
+                    "Criação de um ambiente de trabalho mais eficiente: Considerar fatores ergonômicos como a altura das mesas, a iluminação adequada e a disposição dos equipamentos pode levar a um espaço de trabalho mais funcional, onde as tarefas podem ser realizadas de maneira mais eficiente e com menos esforço físico.",
+                    "Melhora no retorno sobre o investimento (ROI) da empresa: Investir em tecnologias ergonômicas, como cadeiras ajustáveis e suportes para monitores, pode resultar em menor absenteísmo e maior produtividade, oferecendo um retorno positivo sobre o investimento feito na melhoria das condições de trabalho."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Como a integração de equipamentos ergonômicos, como suportes para teclado e mouse, impacta a eficiência e a saúde dos funcionários em um ambiente de escritório?",
+                afirmacao: [
+                    "Diminuição de problemas de saúde: O uso de suportes ergonômicos pode reduzir o risco de condições como síndrome do túnel do carpo e dores no pescoço, promovendo um ambiente de trabalho mais saudável.",
+                    " Aumento da eficiência no trabalho: Funcionários que usam equipamentos ergonômicos tendem a experimentar menos desconforto e fadiga, o que pode levar a um aumento na eficiência e na qualidade do trabalho realizado.",
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "O desenvolvimento ideal para ergonomia envolve a criação de ambientes de trabalho que são cuidadosamente projetados para alinhar as ferramentas e equipamentos às capacidades físicas e cognitivas dos indivíduos. Isso inclui a implementação de soluções personalizadas, como mobiliário ajustável, dispositivos de suporte ergonômico e layouts de trabalho que promovam posturas saudáveis e reduzam o esforço repetitivo. Investir em um desenvolvimento ergonômico eficaz não só previne lesões e desconfortos, mas também maximiza a eficiência, a produtividade e o bem-estar geral dos funcionários, criando um ambiente de trabalho mais sustentável e satisfatór",
+        alternativas: [
+           {
+                texto: "Qual é o impacto da personalização do espaço de trabalho, como a possibilidade de ajustar a altura da cadeira e da mesa, na moral e no desempenho dos funcionários?",
+                afirmacao: [
+                    " Melhoria na moral e satisfação dos funcionários: Permitir que os funcionários ajustem o seu espaço de trabalho de acordo com suas preferências individuais pode aumentar a satisfação no trabalho e a motivação, resultando em uma moral mais alta.",
+                    "Desempenho aprimorado: A personalização do espaço de trabalho pode levar a uma maior eficiência e melhor desempenho, pois os funcionários se sentem mais confortáveis e menos distraídos por desconfortos físicos.",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                afirmacao: [
+                    "Saúde e Segurança: Reduz o risco de lesões relacionadas ao trabalho, como lesões por esforço repetitivo e problemas posturais.",
+                    "Produtividade: Melhora a eficiência e a qualidade do trabalho, pois trabalhadores confortáveis e saudáveis são mais produtivos.",
+                    "Satisfação no Trabalho: Aumenta o bem-estar e a satisfação no trabalho, contribuindo para um ambiente de trabalho mais agradável."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "A ergonomia é um direito sobre seu trabalho, para que durante o periodo que esteja trabalhando você possa ter conforto não so fisico mais mental e estrutural.",
+        alternativas: [
+            {
+                texto: "E agora você pode saber qual são os impactos bons da ergonomia para o futuro.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Utilizar informações próprias traz um toque pessoal ao nosso trabalho. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação.",
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Novas tecnologias trazem novos desafios, por isso após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA.",
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação.",
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "É muito importante refletir sobre os trabalhadores do futuro. Por isso a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA.",
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação.",
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: [
+                    "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
+                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
+                ],
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: [
+                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
+                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
+                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel.",
+                ],
+            },
+        ]
+    }
+];
